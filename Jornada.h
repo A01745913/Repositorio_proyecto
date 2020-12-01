d617c8f67f1afe9e7bad4d0388cf8fe2948a4ef6
JeorgeReds78 <69481052+JeorgeReds78@users.noreply.github.com>
2020-12-01 11:16:28 -0600

    Agregué jornada y casos a main

--- Jornada.h
+++ Jornada.h
@@ -1,1 +1,30 @@
 using namespace std;
+class Jornada{
+    private:
+        int numProducVend;
+        double dineroGanado;
+        int trabajadoresActivos;
+    public:
+        Jornada(){
+            numProducVend = 0;
+            dineroGanado = 0.0;
+            trabajadoresActivos = 0;
+        }
+        Jornada(int numProducVend, double dineroGanado, int trabajadoresActivos){
+            this->numProducVend = numProducVend;
+            this->dineroGanado = dineroGanado;
+            this->trabajadoresActivos = trabajadoresActivos;
+        }
+
+        void estadoActual(){
+            cout << "La cantidad de productos vendidos al momento es: " << numProducVend << endl;
+            cout << "Se ha ganado la cantidad de: " << dineroGanado << "MXN" << endl;
+            cout << "Se encuentran " << trabajadoresActivos << " trabajando actualmente.\n" << endl;
+        }
+        void registrarTrabajadores(int cant){
+            trabajadoresActivos = trabajadoresActivos + cant;
+        }
+        void reducirTrabajadores(int cant){
+            trabajadoresActivos = trabajadoresActivos - cant;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <math.h>
+#include "Moneda.h"
 #include "Prenda.h"
 #include "Cliente.h"
 #include "Trabajador.h"
 #include "Club.h"
+#include "Jornada.h"
 using namespace std;
 
 int main() {
   Trabajador tra;  
   tra.imprimirTrabajador();
+  Jornada jor;
+  jor.estadoActual();
   return 0;
 }
