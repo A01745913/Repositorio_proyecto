9444ea6e979994df301153aab34bef9334752da5
JeorgeReds78 <69481052+JeorgeReds78@users.noreply.github.com>
2020-12-01 20:32:31 -0600

    Mejorar un caso que evite trabajadores activos negativos

--- Jornada.h
+++ Jornada.h
@@ -25,11 +25,25 @@ class Jornada{
             cout << "Se encuentran " << trabajadoresActivos << " trabajando actualmente.\n" << endl;
         }
         void registrarTrabajadores(int cant){
+            if(cant < 0){
+                cant = cant * -1;
+            }
             trabajadoresActivos = trabajadoresActivos + cant;
         }
         void reducirTrabajadores(int cant){
+<<<<<<< Updated upstream
             if(trabajadoresActivos > 0){
                 trabajadoresActivos = trabajadoresActivos - cant;
+=======
+            if(cant < 0){
+                cant = cant * -1;
+            }
+            if(trabajadoresActivos > 0){
+                trabajadoresActivos = trabajadoresActivos - cant;
+                if(trabajadoresActivos < 0){
+                    trabajadoresActivos = 0;
+                }
+>>>>>>> Stashed changes
             }
             else{
                 cout << "Ya no hay trabajadores activos." << endl;
